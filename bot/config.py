9cdfa4b8adee246b88549fc0ec248d4815d6f850
Tlinmo <6a6o4ca@gmail.com>
2024-04-07 18:04:21 +0300

    Switched to a local Bot API server

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
+from aiogram.bot.api import TelegramAPIServer
 
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN, parse_mode='html')
-dp = Dispatcher()
+bot = Bot(token=BOT_TOKEN, parse_mode='html', server=TelegramAPIServer.from_base('http://localhost:8081'))
+dp = Dispatcher(bot)
